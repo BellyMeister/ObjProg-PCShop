efa84eb3e6e428669ef1e9ff7bcaacf3e91581e3
Seppi Damgaard <seppihd@hotmail.com>
2018-05-16 11:03:37 +0200

    Initial Commit

---
+++ ObjProg PCShop/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ObjProg PCShop
+//
+//  Created by elev on 16/05/2018.
+//  Copyright © 2018 Seppi Damgaard. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
